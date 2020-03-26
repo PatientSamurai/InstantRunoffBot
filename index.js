6d6e91a399fb23235c64dcbe385af464c8e6121d
Matt VanderKolk <jmvdkolk@gmail.com>
2020-03-25 20:11:15 -0700

    Switching from discord.io to discord.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var Discord = require('discord.js');
+var auth = require('./auth.json');
+
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('Connected');
+    console.log('Logged in as: ' + bot.user.tag);
+});
+
+bot.on('message', message => {
+    var content = message.content;
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (content.substring(0, 1) == '!') {
+        var args = content.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                message.reply('Pong!');
+                break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
+
+bot.login(auth.token);
