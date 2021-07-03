caaf4ef13ca6d1dd4887d62e125a6e420bd2075c
Matt VanderKolk <jmvdkolk@gmail.com>
2021-07-03 17:23:40 -0400

    Fixing lint errors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import { Client } from 'discord.js';
 import { Bot } from './bot';
+import * as dotenv from 'dotenv';
 
 const client = new Client();
 const bot = new Bot(client);
 
 // Load the bot token
 const botTokenEnvironmentKey: string = 'BOT_TOKEN';
-var token: string | undefined = process.env[botTokenEnvironmentKey];
+let token: string | undefined = process.env[botTokenEnvironmentKey];
 if (!token) {
     console.log(`No bot token found in environment variable ${botTokenEnvironmentKey}.`);
 
     // Fall back the .env file now
-    const dotenv = require('dotenv');
     dotenv.config();
     token = process.env[botTokenEnvironmentKey];
-    
+
     if (!token) {
         console.error('Bot token not found in .env file either.');
     }
